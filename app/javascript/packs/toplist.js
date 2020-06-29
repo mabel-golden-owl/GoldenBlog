f77f62da75df8408470d0a6441dc49afc4ae26e9
mabel <mabel.nguyen.goldenowl@gmail.com>
2020-06-29 16:04:08 +0700

    refactor code

--- app/javascript/packs/toplist.js
+++ app/javascript/packs/toplist.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-  $('.top-posts').on('change', function () {
-    console.log(this.value);
+  $('#choice').on('change', function () {
+    $(this).parents('form').submit();
   })
 })
