d124ebfb1292280dad3d312a357d501d698f39d0
mabel <mabel.nguyen.goldenowl@gmail.com>
2020-07-03 11:40:09 +0700

    fix code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+  before_action :prepare_user, only: %i[show destroy]
+
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def destroy
-    @user = User.find(params[:id])
-
     if @user.destroy
       flash[:notice] = 'User was successfully deleted'
       redirect_back(fallback_location: users_index_path)
     end
+  end
+
+  private
+
+  def prepare_user
+    @user = User.find(params[:id])
   end
 
 end
